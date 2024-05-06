2a705663abaf922c87d1b457e67bd6e1b5fff43f
Luiz <luizdasilvacaita@gmail.com>
2024-05-06 11:26:08 -0300

    criação cadastro.py

---
+++ cadastro.py
@@ -0,0 +1,28 @@
+nome1 = input("digite seu nome:\n")
+idade1 = input("Digite sua idade: \n")
+sexo1 = input("digite seu sexo:\n")
+print("========================================================================")
+nome2 = input("digite seu nome:\n")
+idade2 = input("Digite sua idade: \n")
+sexo2 = input("digite seu sexo:\n")
+print("========================================================================")
+nome3 = input("digite seu nome:\n")
+idade3 = input("Digite sua idade: \n")
+sexo3 = input("digite seu sexo:\n")
+print("========================================================================")
+if idade1 >= "18":
+    print("O nome da primeira pessoa é: ", nome1, ". Ela tem ", idade1, "anos, é do genero ", sexo1, "e é maior de idade")
+else:
+    print("O nome da primeira pessoa é: ", nome1, ". Ela tem ", idade1, "anos, é do genero ", sexo1, "e é menor de idade")
+
+print("========================================================================")
+if idade2 >= "18":
+    print("O nome da segunda pessoa é: ", nome2, ". Ela tem ", idade2, "anos, é do genero ", sexo2, "e é maior de idade")
+else:
+    print("O nome da segunda pessoa é: ", nome2, ". Ela tem ", idade2, "anos, é do genero ", sexo2, "e é menor de idade")
+print("========================================================================")
+if idade3 >= "18":
+    print("O nome da terceira pessoa é: ", nome3, ". Ela tem ", idade3, "anos, é do genero ", sexo3, "e é maior de idade")
+else:
+    print("O nome da terceira pessoa é: ", nome3, ". Ela tem ", idade3, "anos, é do genero ", sexo3, "e é menor de idade")
+
