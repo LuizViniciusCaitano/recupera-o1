62698e74231e352cb654212c900a4651c70a9a25
Luiz <luizdasilvacaita@gmail.com>
2024-05-06 11:14:05 -0300

    criação ordem.py

--- ordem.py
+++ ordem.py
@@ -14,4 +14,4 @@ if n1 != n2 and n1!= n3 and n2 !=n3:
     elif n3 < n2 and n3 < n1 and n1 < n2:
         print(n3, n1, n2)
     elif n3 < n2 and n3 < n1 and n2 < n1:
-        print(n3, n2, n1)
+        print(n3, n2, n1) 
